796526f666845cd452420b4662e18904b6a7cfa4
dimput <dimasputra@mail.ugm.ac.id>
2020-02-22 17:01:32 +0700

    apikey

--- controller.js
+++ controller.js
@@ -109,9 +109,6 @@ exports.deleteTicket = function (req, res) {
 
 // USERS CRUD
 exports.users = function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // If needed
     if (check(req, res)) {
         connection.query('SELECT * FROM employees', function (error, rows, fields) {
             if (error) {
